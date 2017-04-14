b2df80684395ccb0d528605f1e6e0839afcbeeb8
Takamitsu Mizutori <mizutori@gmail.com>
2017-04-14 18:15:35 +0900

    Added privacy policy link to About page

--- ARMusicKit/app/src/main/java/com/yurisuzuki/ActivityAbout.java
+++ ARMusicKit/app/src/main/java/com/yurisuzuki/ActivityAbout.java
@@ -15,6 +15,7 @@ import com.yurisuzuki.playsound.R;
 public class ActivityAbout extends Activity {
     private final static String GPL_LINK = "http://www.gnu.org/licenses/gpl-3.0.en.html";
     private final static String LGPL_LINK = "http://www.gnu.org/licenses/lgpl-3.0.en.html";
+    private final static String PRIVACY_POLICY_LINK = "http://yurisuzuki.com/ar-music-kit-privacy-policy";
 
 
 
@@ -25,11 +26,14 @@ public class ActivityAbout extends Activity {
 
         TextView gplTextView = (TextView)findViewById(R.id.about_gpl_link);
         TextView lgplTextView = (TextView)findViewById(R.id.about_lgpl_link);
+        TextView privacyPolicyTextView = (TextView)findViewById(R.id.about_privacy_policy_link);
 
 
         String gplLinkText = "GPLv3. <a href=\"" + GPL_LINK + "\">" + GPL_LINK + "</a>";
 
         String lgplLinkText = "LGPLv3. <a href=\"" + LGPL_LINK + "\">" + LGPL_LINK + "</a>";
+
+        String privacyPolicyText = "To view our privacy policy, click here. <a href=\"" + PRIVACY_POLICY_LINK + "\">" + PRIVACY_POLICY_LINK + "</a>";
 
 
 
@@ -37,6 +41,11 @@ public class ActivityAbout extends Activity {
         gplTextView.setMovementMethod(LinkMovementMethod.getInstance());
         lgplTextView.setText(Html.fromHtml(lgplLinkText));
         lgplTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        privacyPolicyTextView.setText(Html.fromHtml(privacyPolicyText));
+        privacyPolicyTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+
 
 
     }
